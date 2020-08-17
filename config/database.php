1d442200cc15c7e328280463112cecde326b8622
splaa <splaandrey@gmail.com>
2020-08-17 08:25:10 +0300

    Update path to database/database.sqlite

--- config/database.php
+++ config/database.php
@@ -35,7 +35,8 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            #'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => dirname(__DIR__).'/database/database.sqlite',
             'prefix' => '',
         ],
 
