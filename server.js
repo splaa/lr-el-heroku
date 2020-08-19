ed72a6da48e6f0a21f6b927fff0799760ae5a150
splaa <splaandrey@gmail.com>
2020-08-19 15:49:13 +0300

    Running Laravel Echo Server the right way to Heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const env = process.env;
+
+require('laravel-echo-server').run({
+    authHost: env.APP_URL,
+    devMode: env.APP_DEBUG,
+    database: "redis",
+    databaseConfig: {
+        redis: {
+            host: env.REDIS_HOST,
+            port: env.REDIS_PORT,
+        }
+    }
+});
